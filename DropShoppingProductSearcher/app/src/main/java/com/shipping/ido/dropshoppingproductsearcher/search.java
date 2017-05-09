e5283db289ce230f5cd2f07f59ae2e9774095637
iuyt9003 <iuyt9003@gmail.com>
2017-05-09 03:03:17 +0300

    asdasdas

---
+++ DropShoppingProductSearcher/app/src/main/java/com/shipping/ido/dropshoppingproductsearcher/search.java
@@ -0,0 +1,56 @@
+package com.shipping.ido.dropshoppingproductsearcher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class search extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_search);
+
+
+        // Hide the action bar on the top
+        //ActionBar actionBar = getSupportActionBar();
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3680180972942165~8232293539");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        //actionBar.hide();
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
